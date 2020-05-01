6e3ac47ae1adbbdee38aa274935504c073d887af
yskim <spro1@naver.com>
2020-05-01 19:14:28 +0900

    Updates

---
+++ precache-manifest.a65c7dddf6f41c8e4102303027253f17.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11efb2edd851f3a9e8f7830ed9ebd169",
+    "url": "/sutdy_react/index.html"
+  },
+  {
+    "revision": "3b99da6a4803f7952a73",
+    "url": "/sutdy_react/static/css/2.8487b377.chunk.css"
+  },
+  {
+    "revision": "4763e5cc8db8188fb52e",
+    "url": "/sutdy_react/static/css/main.f4815a6c.chunk.css"
+  },
+  {
+    "revision": "3b99da6a4803f7952a73",
+    "url": "/sutdy_react/static/js/2.ea2f76ac.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/sutdy_react/static/js/2.ea2f76ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4763e5cc8db8188fb52e",
+    "url": "/sutdy_react/static/js/main.5c6d1423.chunk.js"
+  },
+  {
+    "revision": "3e976195c7995f475f83",
+    "url": "/sutdy_react/static/js/runtime-main.5262fe3c.js"
+  },
+  {
+    "revision": "b900da4548a3be9380483c8bfd91e9f2",
+    "url": "/sutdy_react/static/media/car.b900da45.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sutdy_react/static/media/logo.5d5d9eef.svg"
+  }
+]);
